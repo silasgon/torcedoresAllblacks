f141c5104d4adac1e2b7dece1b7ac10c618c0c9e
Silas Gonm <silasidc@gmail.com>
2021-11-21 18:42:34 -0300

    upload

--- editar.php
+++ editar.php
@@ -5,7 +5,6 @@ use \App\Entity\Torcedor;
 
 //Validação do ID
 if(!isset($_GET['id']) or !is_numeric($_GET['id'])){
-    echo "<pre>";echo "Estou no IF";print_r($_GET['id']);echo "</pre>";exit;
     header('location: index.php?status=error');
 }
 
@@ -15,23 +14,26 @@ $torcedor = Torcedor::getTorcedor($_GET['id']);
 
 //validar torcedor
 if (!$torcedor instanceof Torcedor) {
+    echo "<pre>";echo "Verificando a instancia";print_r($torcedor);echo "</pre>";exit;
     header('location: index.php?status=error');
     exit;
 }
 
+echo "<pre>";echo "antes de validando o POST";print_r($torcedor);echo "</pre>";exit;
 
 //Validação post
 if (isset($_POST['nome'], $_POST['documento'], $_POST['email'], $_POST['telefone'], $_POST['cep'], $_POST['endereco'], $_POST['cidade'], $_POST['uf'], $_POST['ativo'] )) {
 
-    $torcedor->nome     = $_POST['nome'];
-    $torcedor->documento     = $_POST['documento'];
-    $torcedor->email     = $_POST['email'];
+    echo "<pre>";echo "no IF de Validação de validando o POST";print_r($torcedor);echo "</pre>";exit;
+    $torcedor->nome         = $_POST['nome'];
+    $torcedor->documento    = $_POST['documento'];
+    $torcedor->email        = $_POST['email'];
     $torcedor->telefone     = $_POST['telefone'];
-    $torcedor->cep     = $_POST['cep'];
+    $torcedor->cep          = $_POST['cep'];
     $torcedor->endereco     = $_POST['endereco'];
-    $torcedor->cidade     = $_POST['cidade'];
-    $torcedor->uf     = $_POST['uf'];
-    $torcedor->ativo     = $_POST['ativo'];
+    $torcedor->cidade       = $_POST['cidade'];
+    $torcedor->uf           = $_POST['uf'];
+    $torcedor->ativo        = $_POST['ativo'];
     
     $torcedor->atualizar();
 
